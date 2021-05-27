1e19499db7415e7a3f289a912b238418f7a93a47
Akoasm6 <763449396@qq.com>
2021-05-27 22:05:21 +0800

    添加接收excel数据接口

--- src/main/java/io/oicp/yorick61c/controller/BodyExamController.java
+++ src/main/java/io/oicp/yorick61c/controller/BodyExamController.java
@@ -3,6 +3,7 @@ package io.oicp.yorick61c.controller;
 
 import io.oicp.yorick61c.domain.body_exam.DataItem;
 import io.oicp.yorick61c.domain.MsgBox;
+import io.oicp.yorick61c.domain.login.User;
 import io.oicp.yorick61c.pojo.dto.body_exam_dto.CItemNameDto;
 import io.oicp.yorick61c.service.BodyExamService;
 import io.oicp.yorick61c.utils.JsonUtil;
@@ -24,6 +25,25 @@ public class BodyExamController {
     @ResponseBody
     public String saveExamDataItemInfo(@RequestBody DataItem info) {
         int insertStatus = bodyExamService.saveDataItem(info);
+        MsgBox msg = new MsgBox();
+        if (insertStatus == 0) {
+            msg.setCode(0);
+            msg.setMsg("保存失败");
+        } else {
+            msg.setCode(1);
+            msg.setMsg("保存成功");
+        }
+        return JsonUtil.obj2String(msg);
+    }
+
+    @PostMapping("/save_excel_item")
+    @ResponseBody
+    public String saveExamExcelItemInfo(@RequestBody List<DataItem> ItemInfo) {
+        int insertStatus = 0;
+        for (DataItem info : ItemInfo){
+            insertStatus = bodyExamService.saveDataItem(info);
+            if (insertStatus == 0){break;}
+        }
         MsgBox msg = new MsgBox();
         if (insertStatus == 0) {
             msg.setCode(0);
