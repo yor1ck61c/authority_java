6772c44018a868133f17def7843642fa2f92f390
yorick_61c <1240371570@qq.com>
2021-07-03 01:40:15 +0800

    update TimeUtils

--- src/main/java/io/oicp/yorick61c/utils/TimeUtil.java
+++ src/main/java/io/oicp/yorick61c/utils/TimeUtil.java
@@ -9,10 +9,12 @@ public class TimeUtil {
         char[] chars = time.toCharArray();
         chars[10] = ' ';
         chars[12] += 8;
-        if (Character.getNumericValue(chars[12]) > 10) {
+        int hours = Character.getNumericValue(chars[12]);
+        if ( hours > 9 || hours == -1) {
             chars[12] -= 10;
             chars[11]++;
         }
+
         return new String(chars).substring(0, 19);
     }
 
@@ -20,4 +22,11 @@ public class TimeUtil {
         SimpleDateFormat sdf =new SimpleDateFormat("yyyy-MM-dd HH:mm:ss" );
         return sdf.format(new Date());
     }
+
+    public static void main(String[] args) {
+        System.out.println(formatTime("2021-06-29T23:51:41.000Z"));
+        // System.out.println(Character.getNumericValue('.'));
+    }
+
+
 }
